3d844325c95728df49f475458f51b7ca9e46d145
DonatasNoreika <donoras@gmail.com>
2026-08-14 08:39:24 +0300

    +Aprašyti Post, Comment modelius, registruoti jį admin.py

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField()
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(to="Post", on_delete=models.CASCADE)
+    content = models.TextField(max_length=1000)
+    date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
