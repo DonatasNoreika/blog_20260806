d852d97568985592a6e774c0fb6c4436c3cb5ce4
DonatasNoreika <donoras@gmail.com>
2026-08-14 08:25:53 +0300

    +pasiruošimo darbai

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ALLOWED_HOSTS
 # Application definition
 
 INSTALLED_APPS = [
+    'blog',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
